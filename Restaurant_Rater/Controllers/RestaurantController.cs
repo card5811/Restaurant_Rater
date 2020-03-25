a05a3b4bedc99d7d95362df5009dfd15731a65d6
card5811 <60365359+card5811@users.noreply.github.com>
2020-03-25 12:03:22 -0400

    edit functionality

--- Restaurant_Rater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using Restaurant_Rater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -62,6 +63,40 @@ namespace Restaurant_Rater.Controllers
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // Get: Restauraunt/Edit/{id}
+        // Get id from user
+        // Handle if id is null
+        //Find restauraunt by id
+        //If Restaurant doesn't exist
+        //Return Restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // Post: Restauraunt/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
